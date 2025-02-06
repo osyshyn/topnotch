9721510e436d505e64431cfd82b9b6ecd5b7ed88
SashaKuh <oleksakukh@gmail.com>
2025-02-06 02:28:55 +0200

    topnotch

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './index.css'
+import { StyledButton } from './styledButton';
+const App = () => {
+  return(
+    <>
+      <h1 className="bg-red-500">hello</h1>
+      <StyledButton>Click Me</StyledButton>
+    </>
+  )
+}
+export default App;
---
+++ src/styledButton.ts
@@ -0,0 +1,14 @@
+// components/Button.tsx
+import styled from 'styled-components';
+
+export const StyledButton = styled.button`
+  background-color: blue;
+  color: white;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+  &:hover {
+    background-color: darkblue;
+  }
+`;
+

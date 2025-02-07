da9f36c7a3e8328f2d79208b4a13526e16a0a6e3
boxing-in-it <tbol240@gmail.com>
2025-02-07 16:03:21 +0200

    add title

---
+++ src/components/PageTitle/index.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from 'react'
+
+interface Props {
+    title: string;
+    subtitle?: string;
+}
+
+const PageTitle:FC<Props> = ({title, subtitle}) => {
+  return (
+    <div className="w-full flex flex-col justify-center items-center">
+        <h1 className='text-[80px] font-[700] text-white'>{title}</h1>
+        <hr className="my-4 w-full border-solid border-gray-400" />
+        <p>{subtitle}</p>
+    </div>
+  )
+}
+
+export default PageTitle
